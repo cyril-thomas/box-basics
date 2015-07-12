15f79f67b8e3d97159c1723a13b436eb9897f90b
Cyril <cyril.satyam@gmail.com>
2015-07-12 17:38:27 -0600

    Seperating admin pages from the main site

---
+++ src/main/java/com/simplyct/woddojo/controller/admin/ScheduleController.java
@@ -0,0 +1,27 @@
+package com.simplyct.woddojo.controller.admin;
+
+import com.simplyct.woddojo.model.Schedule;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by cyril on 6/9/15.
+ */
+@Controller
+@RequestMapping("/schedule")
+public class ScheduleController {
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String schedule() {
+        return "admin/schedule/list";
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public String edit(Model model) {
+        model.addAttribute("schedule", new Schedule());
+
+        return "admin/schedule/edit";
+    }
+}
