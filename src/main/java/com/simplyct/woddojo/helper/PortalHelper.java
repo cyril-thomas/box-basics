89a2a8290e4b9e0c6daa9cd0877ffd855cecc3a5
Cyril <cyril.satyam@gmail.com>
2015-07-20 19:43:52 -0600

    Adding registration form

--- src/main/java/com/simplyct/woddojo/helper/PortalHelper.java
+++ src/main/java/com/simplyct/woddojo/helper/PortalHelper.java
@@ -3,10 +3,14 @@ package com.simplyct.woddojo.helper;
 import com.simplyct.woddojo.helper.dto.*;
 import com.simplyct.woddojo.model.*;
 import com.simplyct.woddojo.repository.*;
+import org.jinstagram.Instagram;
+import org.jinstagram.auth.oauth.InstagramService;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+import javax.servlet.http.HttpSession;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -37,6 +41,12 @@ public class PortalHelper {
 
     @Autowired
     ScheduleRepository scheduleRepository;
+
+    @Autowired
+    SocialHelper socialHelper;
+
+    @Autowired
+    InstagramService instagramService;
 
 
     public HomePage getHomePage(Long orgId) {
@@ -81,5 +91,34 @@ public class PortalHelper {
         return services.stream()
                        .map(e -> new ServiceDetail(e))
                        .collect(Collectors.toList());
+    }
+    
+    public void loadHomePage(Model model, Long orgId, HttpSession httpSession){
+        HomePage homePage = getHomePage(orgId);
+        model.addAttribute("homeObj", homePage);
+
+        AboutPage aboutPage = getAboutPage(orgId);
+        model.addAttribute("aboutObj", aboutPage);
+
+        List<CoachDetail> coaches = getCoaches(orgId);
+        model.addAttribute("coaches", coaches);
+
+        GymDetail gymDetail = getGymDetail(orgId);
+        model.addAttribute("gymObj", gymDetail);
+
+        WODDetail wod = getWOD(orgId);
+        model.addAttribute("wod", wod);
+
+        List<ServiceDetail> services = getServices(orgId);
+        model.addAttribute("services", services);
+
+        if (httpSession.getAttribute("INSTAGRAM_OBJECT") == null) {
+            model.addAttribute("feed", null);
+            model.addAttribute("authorizationUrl", instagramService.getAuthorizationUrl(null));
+        } else {
+
+            Instagram instagram = (Instagram) httpSession.getAttribute("INSTAGRAM_OBJECT");
+            model.addAttribute("feed", socialHelper.getInstagramFeed(instagram, orgId));
+        }
     }
 }
